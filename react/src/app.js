66024d800901d17454e65a2e8f67b618b44f9d9f
Benjamin Clos <benjamin.clos@gmail.com>
2019-03-20 11:15:44 -0400

    fix: add max-width to react demo

--- react/src/app.js
+++ react/src/app.js
@@ -1,6 +1,7 @@
 import React    from "react"
 import ReactDOM from "react-dom"
 import {Player} from "./player"
+import "./styles/app.sass"
 
 const player = React.createElement(Player, 
   { src   : "eedf1500-78f9-4249-977a-974445a09db3"
