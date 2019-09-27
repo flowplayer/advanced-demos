119e94ddb1d87843d8ee49ca4f6cfe91beeb3e99
Benjamin Clos <benjamin.clos@gmail.com>
2019-09-27 12:21:10 -0400

    docs(chapters): add token

--- demos/chapters/src/mocks/video.js
+++ demos/chapters/src/mocks/video.js
@@ -1,4 +1,5 @@
 export default 
-  { src: "https://edge.flowplayer.org/starwreck/hls/playlist.m3u8"
-  , ratio: "21:9"
+  { src   : "https://edge.flowplayer.org/starwreck/hls/playlist.m3u8"
+  , ratio : "21:9"
+  , token : process.env.TOKEN 
   }
