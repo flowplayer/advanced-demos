1f95a74cca4ceb9403bad377f9b2486a970dc847
Niklas Närhinen <niklas@narhinen.net>
2021-02-15 20:57:28 +0200

    wip: float on scroll demo

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,4 @@ parcel build --no-minify --public-url=. -d docs/react demos/react/index.html
 parcel build --no-minify --public-url=. -d docs/chapters demos/chapters/index.html
 parcel build --no-minify --public-url=. -d docs/loader demos/loader/index.html
 parcel build --no-minify --public-url=. -d docs/background-video demos/hls-background/index.html
+parcel build --no-minify --public-url=. -d docs/float-on-scroll demos/float-on-scroll/index.html
