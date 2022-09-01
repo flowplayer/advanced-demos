f9747d4d86284ec1332029d418d7a034d15f5ff2
Mehdi <rashadatjou.mehdi@gmail.com>
2022-09-01 17:38:58 +0200

    [FEATURE] added react demo for version 1.1.1;

---
+++ demos/react/1.1.1/src/config.ts
@@ -0,0 +1,17 @@
+/*
+ * File: config.ts
+ * Project: React Example
+ * Created: Thursday, 1st September 2022
+ * Author: Mehdi Rashadatjou
+ * -----
+ * Copyright 2021 - 2022, ©Flowplayer
+ * -----
+ */
+
+export const DEMO_TOKEN =
+  "eyJraWQiOiJiRmFRNEdUam9lNVEiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJjIjoie1wiYWNsXCI6NixcImlkXCI6XCJiRmFRNEdUam9lNVFcIixcImRvbWFpblwiOltcImJ1aWxkcy5mbG93cGxheWVyLmNvbVwiXX0iLCJpc3MiOiJGbG93cGxheWVyIn0.upfvSSPnB-v2ADHfbWG8ye9jDQhgwnMhZWQUqDS2DOLQbldCt9N8Atbq-gRm4GbqRRS7zoBFvvf6CgYWaV93nw";
+
+export const SOURCES = [
+  "//edge.flowplayer.org/bauhaus.mp4",
+  "//edge.flowplayer.org/functional.mp4"
+];
